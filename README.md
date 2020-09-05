d0f0c81fee2bd4512fb9fee412d0c3cfba3c8836
Noxolo22 <sinoxolonoxolo77@gmail.com>
2020-09-05 22:18:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-ti7vjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ti7vjs)
---
+++ src/app/assets/data.ts
@@ -0,0 +1,51 @@
+export const data = [
+  {
+    "id": "1",
+    "title": "Jean shorts",
+    "description":"hot jean shots",
+    "image": "https://i.pinimg.com/564x/6e/1a/1e/6e1a1e6333507ebdf001ff30aeccc034.jpg",
+    "price" : "46 "
+  },
+  {
+    "id": "2",
+    "title": "White dress",
+    "description": " cute white dress",
+    "image": "https://scylla.mobee.xyz/f_webp/w_420/q_90/https://www.ecstasycoffee.com/wp-content/uploads/2017/05/Summer-Fashion-White-off-the-shoulder-Dress.jpg",
+    "price" : "330"
+  },
+  {
+    "id": "3",
+    "title": "Summer outfit",
+    "description": "Block heel,yellow dress,bag",
+    "image": "https://blog.stitchfix.com/wp-content/uploads/18-05-03_Set_1_W_OLD_v3_SQ.jpg",
+    "price" : "590"
+  },
+  {
+    "id": "4",
+    "title": "Summer cute outfits",
+    "description": "White vest,shorts,bag and sandals",
+    "image": "https://i.pinimg.com/736x/c4/a3/cf/c4a3cfc38980645270cc853271b0daca.jpg",
+    "price" : "460"
+  },
+  {
+    "id": "5",
+    "title": "Summer dress",
+    "description": "Trapezium dress",
+    "image": "https://i0.wp.com/addicfashion.com/wp-content/uploads/2019/06/glamour-summer-fashion-trends-ideas-for-plus-size31.jpg?fit=1024%2C1536&ssl=1",
+    "price" : "800"
+  },
+  {
+    "id": "6",
+    "title": "Jumpsuit",
+    "description": "Short Jumpsuit",
+    "image": "https://d1126yhoonl7p4.cloudfront.net/img/p/1/1/5/1/7/4/115174-large_default.jpg",
+    "price" : "230"
+  },
+  {
+    "id": "7",
+    "title": "Jumpsuit",
+    "description": "Blue long jumpsuit",
+    "image": "https://lh3.googleusercontent.com/proxy/BBrVudSaAJ18YfgIJC_lfc5C9fwKRc4XsUfSs6lIXGhLytNJyEGjeqjaYJt_NlnwtZ5F_g-2kIgfCFvU7RVChdNbqM8IkOIDFDZtJXoIJE3TQG2_tCOYkzgRlHPM02ZBUQFsXIqHy4kZ7E6QxKVNuXXtUUzhiOE0lMjlKLGm0vIXOeE8XJJA3Y980v0kND8NSbl4qsI8CQ4A",
+    "price" : "340"
+  },
+];
